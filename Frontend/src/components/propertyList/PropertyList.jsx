0db58d2802f4c0e18a6c936297ebc3430a5204bb
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-07-19 23:52:12 +0530

    add file

--- Frontend/src/components/propertyList/PropertyList.jsx
+++ Frontend/src/components/propertyList/PropertyList.jsx
@@ -4,31 +4,38 @@ const PropertyList = () => {
     return (
         <div className="pList">
             <div className="pListItem">
-                <img src="" alt="" className="pListImg" />
+                <img src="./images/eche_names/hotel.jpg" alt="" className="pListImg" />
                 <div className="pListTitles">
                     <h1> Hotels </h1>
-                    <h1>233 Hotels</h1>
+                    <h2>233 hotels</h2>
                 </div>
             </div>
             <div className="pListItem">
-                <img src="" alt="" className="pListImg" />
+                <img src="./images/eche_names/apartment.jpg" alt="" className="pListImg" />
                 <div className="pListTitles">
                     <h1> Apartments </h1>
-                    <h1>390 Apartments</h1>
+                    <h2>390 apartments</h2>
                 </div>
             </div>
             <div className="pListItem">
-                <img src="" alt="" className="pListImg" />
+                <img src="./images/eche_names/resort.jpg" alt="" className="pListImg" />
+                <div className="pListTitles">
+                    <h1> Resorts </h1>
+                    <h2>390 resorts</h2>
+                </div>
+            </div>
+            <div className="pListItem">
+                <img src="./images/eche_names/villa.jpg" alt="" className="pListImg" />
                 <div className="pListTitles">
                     <h1> Villas </h1>
-                    <h1>179 Villas</h1>
+                    <h2>179 villas</h2>
                 </div>
             </div>
             <div className="pListItem">
-                <img src="" alt="" className="pListImg" />
+                <img src="./images/eche_names/cabin.jpg" alt="" className="pListImg" />
                 <div className="pListTitles">
                     <h1> Cabins </h1>
-                    <h1>80 Cabins</h1>
+                    <h2>80 Cabins</h2>
                 </div>
             </div>
         </div>
