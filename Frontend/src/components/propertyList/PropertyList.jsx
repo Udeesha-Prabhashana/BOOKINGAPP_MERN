b81b67c66b261b3986f6dda356438bbfb6577ba3
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-07-19 23:06:56 +0530

    update

--- Frontend/src/components/propertyList/PropertyList.jsx
+++ Frontend/src/components/propertyList/PropertyList.jsx
@@ -7,28 +7,28 @@ const PropertyList = () => {
                 <img src="" alt="" className="pListImg" />
                 <div className="pListTitles">
                     <h1> Hotels </h1>
-                    <h1>233 hotels</h1>
+                    <h1>233 Hotels</h1>
                 </div>
             </div>
             <div className="pListItem">
                 <img src="" alt="" className="pListImg" />
                 <div className="pListTitles">
                     <h1> Apartments </h1>
-                    <h1>390 hotels</h1>
+                    <h1>390 Apartments</h1>
                 </div>
             </div>
             <div className="pListItem">
                 <img src="" alt="" className="pListImg" />
                 <div className="pListTitles">
                     <h1> Villas </h1>
-                    <h1>179 hotels</h1>
+                    <h1>179 Villas</h1>
                 </div>
             </div>
             <div className="pListItem">
                 <img src="" alt="" className="pListImg" />
                 <div className="pListTitles">
                     <h1> Cabins </h1>
-                    <h1>80 hotels</h1>
+                    <h1>80 Cabins</h1>
                 </div>
             </div>
         </div>
