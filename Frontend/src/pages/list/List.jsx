c97f5bac072e924bf735058d43c416400bfde8b4
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-07-27 08:38:12 +0530

    add

--- Frontend/src/pages/list/List.jsx
+++ Frontend/src/pages/list/List.jsx
@@ -2,23 +2,27 @@ import "./list.css"
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {format} from "date-fns";
 import { DateRange } from "react-date-range";
 
 const List = () => {
-
+ 
     const location = useLocation();
-    const [destination, setDestination] = useState(location.state.destination)
-    const [date, setDate] = useState(location.state.date)
-    const [openDate, setOpenDate] = useState(false)
-    const [options, setOptions] = useState(location.state.options)
+    const [destination, setDestination] = useState(location.state.destination);
+    const [date, setDate] = useState(location.state.date);
+    const [openDate, setOpenDate] = useState(false);
+    const [options, setOptions] = useState(location.state.option);
 
+    // useEffect(() => {
+    //     if(location.state?.destination) setDestination(location.state.destination)  //thet cord run after run return cord
+    // }, [location.state])
+    
     return (
         <div>
             <Navbar />
             <Header type="list" />
-            {/* <div className="listContainer">
+            <div className="listContainer">
                 <div className="listWrapper">
                     <div className="listSearch">
                         <h1 className="lsTitle">Search</h1>
@@ -59,25 +63,26 @@ const List = () => {
                                 <span className="lsOptionText">
                                     Adult
                                 </span>
-                                <input type="number" className="lsOptionInput" placeholder={options.adult}/>
+                                <input type="number" min={1} className="lsOptionInput" placeholder={options.adult}/>
                             </div>
                             <div className="lsOptionItem">
                                 <span className="lsOptionText">
                                     Children
                                 </span>
-                                <input type="number" className="lsOptionInput" placeholder={options.children}/>
+                                <input type="number" min={0} className="lsOptionInput" placeholder={options.children}/>
                             </div>
                             <div className="lsOptionItem">
                                 <span className="lsOptionText">
                                     Room
                                 </span>
-                                <input type="number" className="lsOptionInput" placeholder={options.room} />
+                                <input type="number" min={1} className="lsOptionInput" placeholder={options.room} />
                             </div>
                         </div>
+                        <button> Search </button>
                     </div>
                     <div className="listResult"></div>
                 </div>
-            </div> */}
+            </div>
         </div>
     )
 }
