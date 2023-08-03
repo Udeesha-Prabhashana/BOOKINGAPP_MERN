b0d1ea43190afc098880484dc442f9d798599a05
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-08-03 19:56:26 +0530

    add

--- Frontend/src/context/AuthContext.js
+++ Frontend/src/context/AuthContext.js
@@ -1,9 +1,9 @@
 import { createContext, useEffect, useReducer } from "react";  //useful when you have data that needs to be accessed by multiple components at different levels in your React component tree.
 
-const getStoredUser = () => {
+const getStoredUser = () => {            //retrieve the user data from the localStorage
   try {
     const storedUser = localStorage.getItem("user");
-    return storedUser ? JSON.parse(storedUser) : null;
+    return storedUser ? JSON.parse(storedUser) : null; //JSON.parse function is used to parse the retrieved value
   } catch (error) {
     console.error("Error parsing stored user data:", error);
     return null;
@@ -11,14 +11,14 @@ const getStoredUser = () => {
 };
 
 const INITIAL_STATE = {
-  user: getStoredUser(),     //JSON.parse(localStorage.getItem("user")) || null,
-  loading: false,
+  user: getStoredUser(), //JSON.parse(localStorage.getItem("user")) || null,
+  loading: false, //application is in a loading state (e.g., waiting for API responses).
   error: null,
 };
 
-export const AuthContext = createContext(INITIAL_STATE);
+export const AuthContext = createContext(INITIAL_STATE);   //used to store and share the state related to user authentication across various components in the application.
 
-const AuthReducer = (state, action) => {
+const AuthReducer = (state, action) => {      //state (representing the current state) and action (representing the type of action dispatched to update the state)
   switch (action.type) {
     case "LOGIN_START":
       return {
