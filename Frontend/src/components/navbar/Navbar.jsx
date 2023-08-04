b94a3e7394b2d46bb4b59495d34d0879a7fdaea0
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-08-04 23:49:04 +0530

    update

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -1,17 +1,23 @@
+import { useContext } from "react"
 import "./navbar.css"
 import {Link} from "react-router-dom"
+import { AuthContext } from "../../context/AuthContext"
 
-const Navbar =({type}) => {
+const Navbar = () => {
+    
+    const { user } = useContext(AuthContext);
     return (
         <div className="navbar">
             <div className="navContainer">
                 <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>  {/*use to after click hear, come to home page*/}
                     <span className="logo">Upbooking</span>
                 </Link>
-                <div className="newTtem">
+                {user ? user.username : (
+                    <div className="newTtem">
                     <button className="navButton">Register</button>
                     <button className="navButton">Login</button>
                 </div>
+                )};
             </div>
         </div>
     )
