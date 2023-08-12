97d626fd43effe16a6ac71cd65a4174901e7cb7d
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-08-12 23:46:16 +0530

    save

--- Backend/controllers/room.js
+++ Backend/controllers/room.js
@@ -68,20 +68,20 @@ export const deleteRoom = async (req, res, next) => {
     }
 };
 
-// export const getRoom = async (req, res, next) => {
-//   try {
-//     const room = await Room.findById(req.params.id);
-//     res.status(200).json(room);
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-
-export const getRooms = async (req, res, next) => {
+export const getRoom = async (req, res, next) => {
   try {
-    const rooms = await Room.find();
-    res.status(200).json(rooms);
+    const room = await Room.findById(req.params.id);
+    res.status(200).json(room);
   } catch (err) {
     next(err);
   }
 };
+
+// export const getRooms = async (req, res, next) => {
+//   try {
+//     const rooms = await Room.find();
+//     res.status(200).json(rooms);
+//   } catch (err) {
+//     next(err);
+//   }
+// };
