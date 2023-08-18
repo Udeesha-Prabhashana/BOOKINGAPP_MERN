3f2d0cbdaaeb435ce654ff8ddd492973787b9a85
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-08-19 00:24:46 +0530

    update

--- Admin/src/components/sidebar/Sidebar.jsx
+++ Admin/src/components/sidebar/Sidebar.jsx
@@ -79,7 +79,10 @@ const Sidebar = () => {
                     </li>
                 </ul>
             </div>
-            <div className="bottom">color options</div>
+            <div className="bottom">      {/*give chose color options in dashbord*/}
+                <div className="colorOption"></div>       
+                <div className="colorOption"></div>
+            </div>
         </div>
     )
 }
