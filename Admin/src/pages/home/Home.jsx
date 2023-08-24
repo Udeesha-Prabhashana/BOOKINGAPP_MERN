d7ea94287d9a1a624f2d375787f0ab6a57f96d58
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-08-24 13:40:19 +0530

    update

--- Admin/src/pages/home/Home.jsx
+++ Admin/src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
 import "./home.scss";
 
 
@@ -9,6 +10,12 @@ const Home = () => {
             <Sidebar />
             <div className="homeContainer">
                 <Navbar />
+                <div className="widgets">
+                    <Widget/>
+                    <Widget/>
+                    <Widget/>
+                    <Widget/>
+                </div>
             </div>
         </div>
     )
