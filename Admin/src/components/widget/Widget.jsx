2762f40de445974ac0fa1fe50110d2eccee6f6a9
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-08-24 15:51:37 +0530

    add

--- Admin/src/components/widget/Widget.jsx
+++ Admin/src/components/widget/Widget.jsx
@@ -1,6 +1,9 @@
 import "./widget.scss";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import AccountBalanceWalletOutlinedIcon from "@mui/icons-material/AccountBalanceWalletOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
 
 const Widget = ({ type }) => {
     let data;
@@ -10,12 +13,38 @@ const Widget = ({ type }) => {
             data = {
                 title: "USERS",
                 isMoney: false,
-                link: "See all users"
-            }
+                link: "See all users",
+                icon: <PersonOutlinedIcon className="icon" />,
+            };
+            break;
+        case "order":
+            data = {
+                title: "ORDER",
+                isMoney: false,
+                link: "View all orders",
+                icon: <ShoppingCartOutlinedIcon className="icon" />,
+            };
+            break;
+        case "erarning":
+            data = {
+                title: "ORDER",
+                isMoney: false,
+                link: "View all orders",
+                icon: <ShoppingCartOutlinedIcon className="icon" />,
+            };
+            break;
+        case "balance":
+            data = {
+                title: "ORDER",
+                isMoney: false,
+                link: "View all orders",
+                icon: <ShoppingCartOutlinedIcon className="icon" />,
+            };
+            break;
+        default:
+            break;
+        
     }
-
-
-
 
     return (
         <div className="widget">
