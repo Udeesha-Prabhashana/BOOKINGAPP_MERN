a1fa2ef1f8cb98618b3f11934a49f87cc8ed2ad7
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-08-24 14:13:21 +0530

    add

--- Admin/src/components/widget/Widget.jsx
+++ Admin/src/components/widget/Widget.jsx
@@ -1,10 +1,22 @@
-import "./widget.scss"
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 
 const Widget = () => {
     return (
         <div className="widget">
-            <div className="left">left</div>
-            <div className="right">right </div>
+            <div className="left">
+                <span className="title">USER </span>
+                <span className="counter">12323 </span>
+                <span className="link">See all user </span>
+            </div>
+            <div className="right">
+                <div className="percentage">
+                    <KeyboardArrowUpIcon />
+                    20 %
+                </div>
+                    <PersonOutlinedIcon className="icon"/>
+            </div>
         </div>
     )
 }
