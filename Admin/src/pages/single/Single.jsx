209349d934c3d6d6fb06030145834a3c00233f3b
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-12-26 16:09:22 +0530

    Close #17

--- Admin/src/pages/single/Single.jsx
+++ Admin/src/pages/single/Single.jsx
@@ -1,6 +1,8 @@
 import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chat";
+import List from "../../components/table/Table";
 
 const Single = () => {
   return (
@@ -41,10 +43,13 @@ const Single = () => {
               </div>
             </div>
           </div>
-          <div className="right">
+        <div className="right">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
           </div>
         </div>
         <div className="bottom">
+            <h1 className="title">Last Transactions</h1>
+          <List/>
         </div>
       </div>
     </div>
