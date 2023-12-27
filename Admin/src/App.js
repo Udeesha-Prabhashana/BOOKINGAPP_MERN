b03e4fb5c8cff48aec452087888a57b8f7d37062
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-12-27 14:56:07 +0530

    Admin authentication completed

--- Admin/src/App.js
+++ Admin/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import List from "./pages/list/List";
@@ -8,11 +8,23 @@ import { productInputs, userInputs } from "./formSource";
 import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
 
 
-  const {darkMode} = useContext(DarkModeContext)
+  const { darkMode } = useContext(DarkModeContext)
+  
+  const ProtectedRoute = ({ children }) => {
+    const { user } = useContext(AuthContext)
+    
+    if (!user) {
+      return <Navigate to="/login" />
+    }
+    else {
+      return children;
+    }
+  }
 
   return (
     <div className={darkMode? "app dark" : "app"}>
@@ -20,8 +32,13 @@ function App() {
         <Routes>
           <Route path="/">
             {/* use to route the pages */}
-            <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
+            <Route index element={
+              <ProtectedRoute>
+                  <Home />
+              </ProtectedRoute>
+            }
+            />
             <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
