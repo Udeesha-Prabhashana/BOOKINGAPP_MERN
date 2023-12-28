9f97b567e85547dd5855682ab239865759635e84
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-12-28 23:04:51 +0530

    Adding a new hotels by the Admin panel

--- Admin/src/pages/newHotel/NewHotel.jsx
+++ Admin/src/pages/newHotel/NewHotel.jsx
@@ -3,12 +3,60 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
+import axios from "axios";
+import useFetch from "../../hooks/useFetch";
 
 const NewHotel = ({ inputs, title }) => {
     
-    const [file, setFile] = useState("");
+    const [files, setFiles] = useState("");
+    const [info, setInfo] = useState({});
+    const [rooms, setRooms] = useState([]);
 
-    console.log(file)
+    const { data, loading, error } = useFetch("/rooms");
+
+    const handleChange = (e) => {
+        setInfo(prev => ({ ...prev, [e.target.id]: e.target.value }));
+    };
+
+    const handleSelect = (e) => {
+        const value = Array.from(
+        e.target.selectedOptions,
+        (option) => option.value
+        );
+        setRooms(value);
+  };
+
+
+    const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      const list = await Promise.all(
+        Object.values(files).map(async (file) => {
+          const data = new FormData();
+          data.append("file", file);
+          data.append("upload_preset", "upload");
+          const uploadRes = await axios.post(
+            "https://api.cloudinary.com/v1_1/dromuhnud/image/upload",
+            data
+          );
+
+          const { url } = uploadRes.data;
+          return url;
+        })
+      );
+
+      const newhotel = {
+        ...info,
+        rooms,
+        photos: list,
+      };
+
+      await axios.post("/hotels", newhotel);
+    } catch (err) {console.log(err)}
+  };
+
+
+    console.log(files)
     return (
         <div className="new"> 
             <Sidebar />
@@ -19,8 +67,8 @@ const NewHotel = ({ inputs, title }) => {
                 <div className="bottom">
                     <div className="left">
                         <img
-                            src={file
-                                    ? URL.createObjectURL(file)
+                            src={files
+                                    ? URL.createObjectURL(files[0])
                                     : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"}
                             alt=""
                         />
@@ -34,7 +82,8 @@ const NewHotel = ({ inputs, title }) => {
                                 <input
                                     type="file"
                                     id="file"
-                                    onChange={e => setFile(e.target.files[0])}
+                                    multiple
+                                    onChange={e => setFiles(e.target.files)}
                                     style={{ display: "none" }}
                                 />
                             </div> 
@@ -42,10 +91,36 @@ const NewHotel = ({ inputs, title }) => {
                             {inputs.map((input) => (
                                 <div className="formInput" key={input.id}>
                                     <label>{input.label}</label>
-                                    <input type={input.type} placeholder={input.placeholder} />
+                                    <input
+                                        id={input.id}
+                                        type={input.type}
+                                        placeholder={input.placeholder}
+                                        onChange={handleChange}
+                                    />
                                 </div>
                             ))}
-                            <button> Send </button>
+                            <div className="formInput" >
+                                <label>Featured</label>
+                                <select id="featured" onChange={handleChange}>
+                                    <option value={false}> No</option>
+                                    <option value={true}> Yes</option>
+                                </select>
+                            </div>
+                            <div className="selectRooms" >
+                                <label>Rooms</label>
+                               <select id="rooms" multiple onChange={handleSelect}>
+                                {loading
+                                ? "loading"
+                                : data &&
+                                    data.map((room) => (
+                                    <option key={room._id} value={room._id}>
+                                        {room.title}
+                                    </option>
+                                    ))}
+                            </select>
+                            </div>
+
+                            <button onClick={handleClick}>Send</button>
                         </form>
                     </div>
                 </div>
