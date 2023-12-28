933f8fd2a4e8468340f4e85ba2f7787e93a87ecd
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2023-12-28 21:27:24 +0530

    Adding a new user by the Admin panel

--- Admin/src/pages/new/New.jsx
+++ Admin/src/pages/new/New.jsx
@@ -3,12 +3,45 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
+import axios from "axios";
 
 const New = ({ inputs, title }) => {
     
     const [file, setFile] = useState("");
+    const [info, setInfo] = useState({});
 
-    console.log(file)
+    const handleChange = (e) => {
+        setInfo(prev => ({ ...prev, [e.target.id]: e.target.value }));
+    };
+
+    const handleClick = async e =>{
+        e.preventDefault()
+        const data = new FormData()
+        data.append("file", file)
+        data.append("upload_preset", "upload")
+        try {
+            const uploadRes = await axios.post(
+                "https://api.cloudinary.com/v1_1/dromuhnud/image/upload",       //for image upload using cloudinary.
+                data
+            );
+
+            const { url } = uploadRes.data;
+
+            const newUser = {
+                ...info,
+                img: url,
+            };
+
+            console.log("AAA", newUser)            
+
+            await axios.post("/auth/register", newUser);
+            
+        } catch (err){
+            console.log(err)
+        }
+    }
+
+    console.log(info)
     return (
         <div className="new"> 
             <Sidebar />
@@ -42,10 +75,15 @@ const New = ({ inputs, title }) => {
                             {inputs.map((input) => (
                                 <div className="formInput" key={input.id}>
                                     <label>{input.label}</label>
-                                    <input type={input.type} placeholder={input.placeholder} />
+                                    <input
+                                        onChange={handleChange}
+                                        type={input.type}
+                                        placeholder={input.placeholder}
+                                        id={input.id}
+                                    />
                                 </div>
                             ))}
-                            <button> Send </button>
+                            <button onClick={handleClick}> Send </button>
                         </form>
                     </div>
                 </div>
